1307ebecd8b708624ab523f93fb72ec2e3aa419a
zhangxianglink <zhangxianglink@163.com>
2019-08-26 15:27:49 +0800

    merge ok

--- src/main/java/optimize/ListAndMap.java
+++ src/main/java/optimize/ListAndMap.java
@@ -12,18 +12,7 @@ public class ListAndMap {
 	 */
 	@Test
 	public void Test() {
-		ArrayList<Object> arrayList = new ArrayList<>();
-=======
-import org.junit.Test;
 
-public class ListAndMap {
-	
-	/**
-	 * 三种list的实现, ArrayList, Vector, LinkedList
-	 */
-	@Test
-	public void Test() {
-		
 
 	}
 
