5c770e621ca541b31a7788750a1f59a0544cbece
zhangxianglink <zhangxianglink@163.com>
2019-09-07 22:07:12 +0800

    create thread

---
+++ src/main/java/threads/CreateThreads.java
@@ -0,0 +1,30 @@
+package threads;
+
+import java.util.Vector;
+
+import org.junit.Test;
+
+public class CreateThreads implements Runnable{
+	
+	@Test
+	public void method1() {
+		Thread t1  = new Thread() {
+			@Override
+			public void run() {
+				System.out.println("I am fine!");
+			}
+		};
+		t1.start();
+	}
+	
+
+	@Override
+	public void run() {
+		System.out.println("I am runnable");
+	}
+
+	public static void main(String[] args) {
+		Thread thread = new Thread(new CreateThreads());
+		thread.start();
+	}
+}
